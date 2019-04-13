5eb8fb8d21e6b9bdc9fed91cd7a7167156740241
Moritz Konarski <konarski_m@auca.kg>
2019-04-13 14:12:14 +0600

    do uri 1036, start 1038

--- URI/URI_1036/main.c
+++ URI/URI_1036/main.c
@@ -12,9 +12,9 @@ int main() {
     if (a == 0 || d < 0) {
         printf("Impossivel calcular\n");
     } else {
-        printf("R1 = %.5lf\n", (-b + sqrt(d)) / 2 * a);
-        printf("R2 = %.5lf\n", (-b - sqrt(d)) / 2 * a);
+        printf("R1 = %.5lf\n", (-b + sqrt(d)) / (2 * a));
+        printf("R2 = %.5lf\n", (-b - sqrt(d)) / (2 * a));
     }
 
-    return 0;
+    return 1;
 }
