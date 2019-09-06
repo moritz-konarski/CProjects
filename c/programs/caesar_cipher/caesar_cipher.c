dcdd6203afbfe73a6222e864b98c8f4ff5041f4e
Moritz Konarski <konarski_m@auca.kg>
2019-09-06 22:46:08 +0600

    clean up caesar directory, improve program

--- c/programs/caesar_cipher/caesar_cipher.c
+++ c/programs/caesar_cipher/caesar_cipher.c
@@ -6,22 +6,42 @@
 
 void encrypt(char input[], int16_t shift, char output[]);
 void decrypt(char input[], int16_t shift, char output[]);
+void process_simple_command(char action, int16_t shift);
+
+
+char input[500], output[500];
 
 int main(uint8_t argc, char *argv[]) {
     //the input and output strings
-    char input[500], output[500];
 
     if (argc == 1) {
         printf("Please enter the plain text: ");
-        scanf("%s", input);
+        scanf("%[^\n]s", input);
 
         encrypt(input, SHIFT, output);
         printf("The encrypted text is: %s\n", output);
 
     } else if (argc == 2) {
-        //TODO: implement full file encryption
-        printf("Feature not yet available.\n");
+        for (uint8_t i = 1; i < argc; i++) {
+            if (argv[i][0] == '-') {
+                if (argv[i][1] == 'e') {
+                    printf("Please enter the plain text: ");
+                    scanf("%[^\n]s", input);
 
+                    encrypt(input, SHIFT, output);
+                    printf("The encrypted text is: %s\n", output);
+
+                } else if (argv[i][1] == 'd') {
+                    printf("Please enter the encrypted text: ");
+                    scanf("%[^\n]s", input);
+
+                    decrypt(input, SHIFT, output);
+                    printf("The plain text is: %s\n", output);
+
+                }
+                break;
+            }
+        }
     } else if (argc == 3) {
         //find out which command it is
         for (uint8_t i = 1; i < argc; i++) {
@@ -37,6 +57,19 @@ int main(uint8_t argc, char *argv[]) {
 
 
     return 0;
+}
+
+void process_simple_command(char action, int16_t shift) {
+    if (action == 'e') {
+        printf("Please enter the plain text: ");
+        encrypt(input, shift, output);
+        printf("The encrypted text is: %s\n", output);
+    } else if (action == 'd') {
+        printf("Please enter the encrypted text: ");
+        scanf("%[^\n]s", input);
+        decrypt(input, shift, output);
+        printf("The plain text is: %s\n", output);
+    }
 }
 
 void encrypt(char input[], int16_t shift, char output[]) {
