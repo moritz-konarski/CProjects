1448917d427d687c66bd14f7943991caf95c4180
Moritz Konarski <konarski_m@auca.kg>
2019-11-16 16:01:07 +0600

    start base64 conversion

--- c/programs/base64/base64.c
+++ c/programs/base64/base64.c
@@ -5,38 +5,53 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define STD_SHIFT    3
 #define STR_LEN     500
 #define NAME_LEN    100
-#define FIRST_CHAR  ' '
-#define LAST_CHAR   '~'
+#define INITIAL_CHAR ' '
 #define RANGE LAST_CHAR - FIRST_CHAR
 #define ARG_CHAR    '-'
 
-#define ENCRYPT_ARG 'e'
-#define DECRYPT_ARG 'd'
-#define SHIFT_ARG   's'
 #define INPUT_ARG   'i'
 #define OUTPUT_ARG  'o'
+#define ENCODE_ARG  'e'
+#define DECODE_ARG  'd'
 
-#define ALL_ARGS    "edi:o:s:"
+#define ALL_ARGS    "edi:o:"
 
 #define EMPTY_STR   "\0"
 
 struct Arguments {
-    int shift;
-    bool do_encryption;
+    bool encode;
     char input_file_name[NAME_LEN], 
          output_file_name[NAME_LEN];
 };
 
-void caesar_shift(struct Arguments, char input[], char output[]);
-bool is_number(char str[]);
+void base64_encode(struct Arguments, char input[], char output[]);
+int encode_triplet(char input[], char output[]);
+char get_char(uint8_t num); 
 
 int main(uint8_t argc, char *argv[]) {
 
+    char text[10] = "Man";
+    
+    printf("%s\n", text);
+            uint32_t triple = 0;
+            
+            for (uint8_t i = 3; i > 0 ; --i) {
+                printf("%c\n", text[-i+3]);
+                printf("%x\n", text[-i+3]);
+                printf("%lx\n", text[-i+3] << (0x08 * i));
+                triple = triple | (text[-i+3] << (0x08 * i));
+            }
+            printf("%x\n", triple);
+    char output[20];
+
+    encode_triplet(text, output);
+    printf("%s\n", output);
+
+
+    /*
     struct Arguments args = {
-        STD_SHIFT, 
         true, 
         EMPTY_STR, 
         EMPTY_STR
@@ -46,11 +61,11 @@ int main(uint8_t argc, char *argv[]) {
 
     while ((opt = getopt(argc, argv, ALL_ARGS)) != -1) {
         switch (opt) {
-            case ENCRYPT_ARG:
-                args.do_encryption = true;
+            case ENCODE_ARG:
+                args.encode = true;
                 break;
-            case DECRYPT_ARG: 
-                args.do_encryption = false;
+            case DECODE_ARG: 
+                args.encode = false;
                 break;
             case INPUT_ARG:
                 if (strlen(optarg) < sizeof(args.input_file_name)) {
@@ -68,21 +83,11 @@ int main(uint8_t argc, char *argv[]) {
                     return 1;
                 }
                 break;
-            case SHIFT_ARG:
-                if (is_number(optarg)){
-                    sscanf(optarg, "%d", &args.shift);
-                } else {
-                    fprintf(stderr, "%s: shift must be an integer\n", argv[0]);
-                    return 1;
-                }
-                break;
             default:
-                fprintf(stderr, "Usage: %s [-io] filename [-s] shift [-ed]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-io] filename [-ed] encode/decode\n", argv[0]);
             break;
         }
     }
-
-    //printf("%d | %d | %s - %s |\n", args.shift, args.do_encryption, args.input_file_name, args.output_file_name);
 
     char input[STR_LEN], output[STR_LEN];
     if (strlen(args.input_file_name) >= 3) {
@@ -126,28 +131,29 @@ int main(uint8_t argc, char *argv[]) {
             printf("The result is: \"%s\"\n", output);
         }
     }
+*/
 
     return 0;
 }
 
-bool is_number(char str[]) {
-    if (*str == '-' || (*str >= '0' && *str <= '9')) {
-        ++str;
-        while (*str != 0) {
-            if (!(*str >= '0' && *str <= '9')) {
-                return false;
-            }
-            ++str;
-        }
-        return true;
+char get_char(uint8_t num) {
+    if(num <= 25) {
+        return num + 'A';
+    } else if (num <= 51) {
+        return num - 26 + 'a';
+    } else if (num <= 61) {
+        return num - 52 + '0';
+    } else if (num == 62) {
+        return '+';
     } else {
-        return false;
+        return '/';
     }
 }
 
-void caesar_shift(struct Arguments args, char input[], char output[]) {
+/*
+void base64_encode(struct Arguments args, char input[], char output[]) {
     int result,
-        sign = args.do_encryption ? 1 : -1;
+        sign = args.encode ? 1 : -1;
     while (*input != 0) {
         if (*input >= FIRST_CHAR && *input <= LAST_CHAR) {
             result = ((*input - FIRST_CHAR) + sign * args.shift);
@@ -162,4 +168,45 @@ void caesar_shift(struct Arguments args, char input[], char output[]) {
         output++;
     }
     *output = 0;
+
+
+    //take three chars as input
+    char result[4];
+    //output 4 chars
+    uint32_t triple;
+    while (*input != 0) {
+        if (*input >= FIRST_CHAR && *input <= LAST_CHAR) {
+            triple = 0;
+            for (uint8_t i = 3; i > 0 ; --i) {
+                triple = triple | (*input << (0x08 * i));
+                input++;
+            }
+
+        } else {
+            *output = *input;
+        }
+        input++;
+        output++;
+    }
+    *output = 0;
+}
+*/
+
+int encode_triplet(char input[], char output[]) {
+    if (strlen(input) == 3) {
+        char result[4];
+        uint32_t triple = 0;
+        for (uint8_t i = 3; i > 0 ; --i) {
+            triple = triple | (*input << (0x08 * (i - 1)));
+            input++;
+        }
+        printf("func: %x\n", triple);
+        for (uint8_t j = 0; j < 4; ++j) {
+            output[j] = get_char((triple >> ((3 - j) * 0x06)) & 0x3f);
+            printf("%d\n", (triple >> ((3 - j) * 0x06)) & 0x3f);
+        }
+    } else {
+        return 1;
+    }
+    return 0;
 }
