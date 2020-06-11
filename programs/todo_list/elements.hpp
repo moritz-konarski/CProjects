07623deaa95edfd17023520d9fb10c6bb3d32ccc
Moritz Konarski <konarski_m@auca.kg>
2020-06-11 20:38:47 +0600

    start work with ncurses

--- programs/todo_list/elements.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ELEMENTS_HPP
-#define ELEMENTS_HPP
-
-#include "elements/list_item.hpp"
-#include "elements/list_section.hpp"
-#include "elements/todo_list.hpp"
-
-#endif
--- programs/todo_list/main.cpp
+++ programs/todo_list/main.cpp
@@ -1,40 +1,50 @@
 #include <iostream>
 #include <fstream>
-#include "elements.hpp"
+#include "list_elements.hpp"
+#include "io_elements.hpp"
+#include <ncurses.h>
 
 int main(int argc, char **argv) {
 
+    io_elements::initialize_renderer();
+
+    //io_elements::game_test();
+
+    io_elements::print_line(10, "Hello World", io_elements::Color_Combination::WHITE_BLACK);
+    io_elements::print_line(11, "Hello World Again", io_elements::Color_Combination::BLACK_WHITE);
+    getch();
+    io_elements::highlight_line(10, io_elements::Color_Combination::BLACK_WHITE);
+    getch();
+
+
+
+    /*
+    printw("Hello, World!");
+    refresh();
+    getch();
     using namespace std;
+
+    auto todo_list = std::make_shared<ToDo_List>();
+    Command_Type command = None;
+
+    cout << "Welcome to ToDo List" << endl;
+
+    while(command != Exit) {
+        command = process_input();
+
+        process_command(todo_list, command);
+    }
 
     // create a basic window that pops up
     // file reading class
 
-    /*
     std::ofstream myfile;
     myfile.open ("../lists/example.txt");
     myfile << "Writing this to a file.\n";
     myfile.close();
-     */
+    */
 
-    auto todo_list = make_shared<ToDo_List>();
-    string name;
-
-    cout << "Enter list name: ";
-    cin >> name;
-    cout << endl;
-
-    auto list = todo_list->create_new_section("test _section");
-
-    list->set_name(name);
-    list->create_item("First List_Item");
-    list->create_item("Second List_Item");
-    list->create_sub_item(list->get_items().at(0), "First Sub List_Item");
-    list->create_sub_item(list->get_items().at(0), "Second Sub List_Item");
-    list->create_sub_item(list->get_items().at(0)->get_children().at(0), "First Sub Sub List_Item");
-
-    list->get_items().at(0)->get_children().at(0)->set_checked(true);
-
-    todo_list->print();
+    io_elements::close_renderer();
 
     return 0;
 }
